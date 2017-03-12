fab3e9b0b7bc2148ed898a7f2aba1a1ec39866bf
Github <shusain93>
2017-03-12 16:44:05 -0600

    Generalized config

---
+++ app/src/main/java/eu/aero2x/andromedab/APP_CONSTANTS_EXAMPLE_CHANGE_ME.java
@@ -0,0 +1,9 @@
+package eu.aero2x.andromedab;
+
+/**
+ * Created by Salman on 3/12/17.
+ */
+
+public class APP_CONSTANTS_EXAMPLE_CHANGE_ME {
+    public static String API_URL = "http://services.aero2x.eu:8735"; //API URL WITHOUT THE TRAILING SLASH. Example: http://yourdomain:port
+}
