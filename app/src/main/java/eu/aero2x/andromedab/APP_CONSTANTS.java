137a009adb6524ca9a84e37df61ce96f921c5d78
Github <shusain93>
2017-04-28 11:53:28 -0600

    Removed need to manually configure through code. Now you can setup at runtime

--- app/src/main/java/eu/aero2x/andromedab/APP_CONSTANTS.java
+++ app/src/main/java/eu/aero2x/andromedab/APP_CONSTANTS.java
@@ -4,8 +4,7 @@ package eu.aero2x.andromedab;
  * Created by Salman on 3/12/17.
  */
 
-public class APP_CONSTANTS_EXAMPLE_CHANGE_ME {
-    public static String API_URL = "http://ENTER_YOUR_DOMAIN_HERE:8735"; //API URL WITHOUT THE TRAILING SLASH. Example: http://yourdomain:port
+public class APP_CONSTANTS {
+    public static String API_URL = ""; //API URL WITHOUT THE TRAILING SLASH. Example: http://yourdomain:port
     public static String API_PROTECTION_TOKEN = ""; //The random token/password you configured in the CONFIG.plist in the MessageProxy. This authenticates this build of the app with the server. Don't share this token!
-
 }
